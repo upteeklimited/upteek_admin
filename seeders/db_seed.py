f4b275e160017c51cc1bfa6fe877b6a9a76d8c9c
Jorn Kalu <kiceej@gmail.com>
2025-03-27 16:13:01 +0100

    Fixing errors

--- seeders/db_seed.py
+++ seeders/db_seed.py
@@ -4,13 +4,19 @@ from seeders.user_seeder import run_user_seeder
 from modules.utils.tools import truncate_all_tables
 
 def run_seed(db: Session):
-    tru = truncate_all_tables(db=db)
-    if tru['status'] == False:
-        return tru
-    else:
-        print(run_geo_seeder(db=db))
-        print(run_user_seeder(db=db))
+    try:
+        tru = truncate_all_tables(db=db)
+        if tru['status'] == False:
+            return tru
+        else:
+            print(run_geo_seeder(db=db))
+            print(run_user_seeder(db=db))
+            return {
+                'status': True,
+                'message': 'Seeders ran successfully!'
+            }
+    except Exception as e:
         return {
-            'status': True,
-            'message': 'Seeders ran successfully!'
+            'status': False,
+            'message': str(e)
         }
