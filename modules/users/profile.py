7475b12587b65b6ca8209e31f224985d33fb118e
Jorn Kalu <kiceej@gmail.com>
2025-04-02 22:19:50 +0100

    Added profile module

--- modules/users/profile.py
+++ modules/users/profile.py
@@ -1,7 +1,10 @@
 from typing import Dict
 from sqlalchemy.orm import Session
-from database.model import update_profile_by_user_id, update_setting_by_user_id
+from database.model import update_profile_by_user_id, update_setting_by_user_id, get_single_user_by_id, update_user
 from modules.utils.tools import process_schema_dictionary
+from modules.utils.auth import AuthHandler
+
+auth = AuthHandler()
 
 
 def update_user_profile_details(db: Session, user_id: int=0, values: Dict={}):
@@ -12,4 +15,26 @@ def update_user_profile_details(db: Session, user_id: int=0, values: Dict={}):
         'message': 'Success'
     }
 
-# def update_user
+def update_user_password(db: Session, user_id: int=0, password: str=None, old_password: str=None):
+    user = get_single_user_by_id(db=db, id=user_id)
+    if user is None:
+        return {
+            'status': False,
+            'message': 'User not found',
+        }
+    else:
+        if auth.verify_password(plain_password=old_password, hashed_password=user.password) == False:
+            return {
+                'status': False,
+                'message': 'Old Password Incorrect'
+            }
+        else:
+            password = auth.get_password_hash(password=password)
+            da = {
+                'password': password
+            }
+            update_user(db=db, id=user_id, values=da)
+            return {
+                'status': True,
+                'message': 'Success'
+            }
