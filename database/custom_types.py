777b930806cf0abf2d3197937e56453188a4c9bc
Jorn Kalu <kiceej@gmail.com>
2025-04-27 10:53:05 +0100

    Updated seeder

--- database/custom_types.py
+++ database/custom_types.py
@@ -6,7 +6,7 @@ class JSONText(TypeDecorator):
     cache_ok = True  # Optional but recommended for SQLAlchemy 1.4+
 
     def process_bind_param(self, value, dialect):
-        return json.dumps(value) if value is not None else '{}'
+        return json.dumps(value) if value is not None else None
 
     def process_result_value(self, value, dialect):
         if not value:
