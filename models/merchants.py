5ef718914f1976cc322adb65dbf641eb56b34247
Jorn Kalu <kiceej@gmail.com>
2025-05-13 14:48:08 +0100

    changed seed

--- models/merchants.py
+++ models/merchants.py
@@ -49,7 +49,7 @@ class Merchant(Base):
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now())
     updated_at = Column(TIMESTAMP(timezone=True), nullable=True, onupdate=func.now())
 
-    user = relationship('User')
+    user = relationship('User', back_populates='merchant')
     category = relationship('MerchantCategory')
     currency = relationship('Currency')
 
