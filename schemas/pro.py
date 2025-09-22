4e65f573bb1939cc08f4381d00569e2a237cfd44
Jorn Kalu <kiceej@gmail.com>
2025-09-22 16:16:17 +0100

    22nd Sept 2025

--- schemas/pro.py
+++ schemas/pro.py
@@ -1,16 +1,28 @@
 from typing import Optional
+from fastapi import Form
 from pydantic import BaseModel
 
 class UpdateBasicProfileRequestModel(BaseModel):
     first_name: Optional[str] = None
     other_name: Optional[str] = None
     last_name: Optional[str] = None
+    phone_number: Optional[str] = None
     date_of_birth: Optional[str] = None
     gender: Optional[str] = None
     bio: Optional[str] = None
+    merchant_category_id: Optional[int] = None
+    merchant_currency_id: Optional[int] = None
+    merchant_name: Optional[str] = None
+    merchant_trading_name: Optional[str] = None
+    merchant_description: Optional[str] = None
+    merchant_email: Optional[str] = None
+    merchant_phone_number: Optional[str] = None
     
     class Config:
         orm_mode = True
+
+def parse_update_basic_profile_payload(fields: str = Form(...)) -> UpdateBasicProfileRequestModel:
+    return UpdateBasicProfileRequestModel.model_validate_json(fields)
 
 class UpdatePasswordRequestModel(BaseModel):
     password: str
@@ -22,6 +34,174 @@ class UpdatePasswordRequestModel(BaseModel):
 class UpdateSettingsRequestModel(BaseModel):
     email_notification: Optional[int] = None
     sms_notification: Optional[int] = None
+    dashboard_state: Optional[str] = None
+    interest_state: Optional[str] = None
+    
+    class Config:
+        orm_mode = True
+
+class UpdateAddressRequestModel(BaseModel):
+    state_id: Optional[int] = None
+    city_id: Optional[int] = None
+    lga_id: Optional[int] = None
+    house_number: Optional[str] = None
+    street: Optional[str] = None
+    nearest_bus_stop: Optional[str] = None
+    latitude: Optional[str] = None
+    longitude: Optional[str] = None
+    is_primary: Optional[int] = None
+    
+    class Config:
+        orm_mode = True
+        
+class CreateAddressRequestModel(BaseModel):
+    state_id: Optional[int] = 0
+    city_id: Optional[int] = 0
+    lga_id: Optional[int] = 0
+    house_number: Optional[str] = None
+    street: Optional[str] = None
+    nearest_bus_stop: Optional[str] = None
+    latitude: Optional[str] = None
+    longitude: Optional[str] = None
+    is_primary: Optional[int] = 0
+    
+    class Config:
+        orm_mode = True
+
+class AddressModel(BaseModel):
+    id: int
+    country_id: int
+    state_id: Optional[int] = None
+    city_id: Optional[int] = None
+    lga_id: Optional[int] = None
+    addressable_type: str
+    addressable_id: int
+    house_number: Optional[str] = None
+    street: Optional[str] = None
+    nearest_bus_stop: Optional[str] = None
+    latitude: Optional[str] = None
+    longitude: Optional[str] = None
+    is_primary: int = 0
+    
+    class Config:
+        orm_mode = True
+
+class AddressResponseModel(BaseModel):
+    status: bool
+    message: str
+    data: Optional[AddressModel] = None
+    
+    class Config:
+        orm_mode = True
+
+class SmileIDPayloadRequestModel(BaseModel):
+    job_id: str
+    
+    class Config:
+        orm_mode = True
+
+class SmileIDGenerateWebTokenRequestModel(BaseModel):
+    product: str
+    callback_url: str
+    
+    class Config:
+        orm_mode = True
+
+class SmileIDWebTokenModel(BaseModel):
+    token: str
+    job_id: str
+    
+    class Config:
+        orm_mode = True
+
+class SmileIDWebTokenResponse(BaseModel):
+    status: bool
+    message: str
+    data: Optional[SmileIDWebTokenModel] = None
+    
+    class Config:
+        orm_mode = True
+
+class SmileIDComplainceFinaliseModel(BaseModel):
+    card_back_url: str
+    card_image_url: str
+    selfie_image_url: str
+    
+    class Config:
+        orm_mode = True
+
+class SmileIDComplainceFinaliseResponse(BaseModel):
+    status: bool
+    message: str
+    data: Optional[SmileIDComplainceFinaliseModel] = None
+    
+    class Config:
+        orm_mode = True
+
+class SmileIDMerchantComplainceFinaliseModel(BaseModel):
+    certificate_url: str
+    
+    class Config:
+        orm_mode = True
+
+class SmileIDMerchantComplainceFinaliseResponse(BaseModel):
+    status: bool
+    message: str
+    data: Optional[SmileIDMerchantComplainceFinaliseModel] = None
+    
+    class Config:
+        orm_mode = True
+
+class VerifyBVNRequestModel(BaseModel):
+    bvn: str
+    first_name: str
+    last_name: str
+    phone_number: str
+    date_of_birth: str
+    
+    class Config:
+        orm_mode = True
+
+class VerifyNINRequestModel(BaseModel):
+    nin: str
+    first_name: str
+    last_name: str
+    phone_number: str
+    
+    class Config:
+        orm_mode = True
+
+class BVNMatchModel(BaseModel):
+    bvn: bool
+    name: bool
+    phone_number: bool
+    date_of_birth: bool
+    
+    class Config:
+        orm_mode = True
+
+class NINMatchModel(BaseModel):
+    nin: bool
+    name: bool
+    phone_number: bool
+    
+    class Config:
+        orm_mode = True
+
+class VerifyBVNResposeModel(BaseModel):
+    status: bool
+    message: str
+    data: Optional[BVNMatchModel] = None
+    status_code: Optional[str] = None
+    
+    class Config:
+        orm_mode = True
+
+class VerifyNINResponseModel(BaseModel):
+    status: bool
+    message: str
+    data: Optional[NINMatchModel] = None
+    status_code: Optional[str] = None
     
     class Config:
         orm_mode = True
--- schemas/user.py
+++ schemas/user.py
@@ -40,6 +40,9 @@ class UserModel(BaseModel):
     user_type: Optional[int] = 0
     role: Optional[int] = 0
     is_new_user: Optional[bool] = None
+    has_merchant: Optional[bool] = None
+    pin_available: Optional[bool] = None
+    created_at: Optional[datetime] = None
     
     class Config:
         orm_mode = True
@@ -75,6 +78,8 @@ class SettingModel(BaseModel):
     user_id: int
     email_notification: Optional[int] = 0
     sms_notification: Optional[int] = 0
+    dashboard_state: Optional[str] = None
+    interest_state: Optional[str] = None
     
     class Config:
         orm_mode = True
@@ -86,6 +91,7 @@ class MerchantModel(BaseModel):
     currency_id: Optional[int] = 0
     name: Optional[str] = None
     trading_name: Optional[str] = None
+    slug: Optional[str] = None
     description: Optional[str] = None
     email: Optional[str] = None
     phone_number_one: Optional[str] = None
@@ -93,11 +99,15 @@ class MerchantModel(BaseModel):
     opening_hours: Optional[str] = None
     closing_hours: Optional[str] = None
     logo: Optional[str] = None
+    banner: Optional[str] = None
     thumbnail: Optional[str] = None
     certificate: Optional[str] = None
     memorandum: Optional[str] = None
     utility_bill: Optional[str] = None
     building: Optional[str] = None
+    accept_vat: Optional[int] = None
+    accept_wht: Optional[int] = None
+    status: Optional[int] = None
     compliance_status: Optional[int] = None
     
     class Config:
@@ -113,24 +123,110 @@ class UserMainModel(BaseModel):
     user_type: Optional[int] = 0
     role: Optional[int] = 0
     status: Optional[int] = 0
-    created_at: Optional[str] = None
+    created_at: Optional[datetime] = None
     country: Optional[CountryModel] = None
     profile: Optional[ProfileModel] = None
     merchant: Optional[MerchantModel] = None
+    created_at: Optional[datetime] = None
     
     class Config:
         orm_mode = True
-
-class AuthResponseModel(BaseModel):
+        
+class UserInfoModel(BaseModel):
     id: int
-    access_token: Optional[str] = None
+    country_id: Optional[int] = 0
+    merchant_id: Optional[int] = 0
     username: Optional[str] = None
     phone_number: Optional[str] = None
     email: Optional[str] = None
     user_type: Optional[int] = 0
     role: Optional[int] = 0
+    status: Optional[int] = 0
+    created_at: Optional[datetime] = None
+    profile: Optional[ProfileModel] = None
+    created_at: Optional[datetime] = None
+    
+    class Config:
+        orm_mode = True
+
+class UserInfoResponseModel(BaseModel):
+    status: bool
+    message: str
+    data: Optional[UserInfoModel] = None
+    
+    class Config:
+        orm_mode = True
+
+
+class AuthFinancialInstitutionModel(BaseModel):
+    id: int
+    name: Optional[str] = None
+    icon: Optional[str] = None
+    status: Optional[str] = None
+
+    class Config:
+        orm_mode = True
+
+class AuthVirtualAccountModel(BaseModel):
+    id: int
+    user_id: Optional[int] = 0
+    account_id: Optional[int] = 0
+    financial_institution_id: Optional[int] = 0
+    account_name: Optional[str] = None
+    account_number: Optional[str] = None
+    bank_name: Optional[str] = None
+    is_primary: Optional[int] = 0
+    status: Optional[int] = 0
+    created_at: Optional[datetime] = None
+    financial_institution: Optional[AuthFinancialInstitutionModel] = None
+
+    class Config:
+        orm_mode = True
+
+class AuthAccountModel(BaseModel):
+    id: int
+    user_id: int
+    account_type_id: Optional[int] = 0
+    account_name: Optional[str] = None
+    account_number: Optional[str] = None
+    nuban: Optional[str] = None
+    provider: Optional[str] = None
+    available_balance: Optional[float] = 0.0
+    ledger_balance: Optional[float] = 0.0
+    sms_notification: Optional[int] = 0
+    email_notification: Optional[int] = 0
+    is_primary: Optional[int] = 0
+    virtual_account: Optional[AuthVirtualAccountModel] = None
+    
+    class Config:
+        orm_mode = True
+
+class AddressModel(BaseModel):
+    id: int
+    country_id: Optional[int] = 0
+    state_id: Optional[int] = 0
+    city_id: Optional[int] = 0
+    lga_id: Optional[int] = 0
+    addressable_type: Optional[str] = None
+    addressable_id: Optional[int] = 0
+    house_number: Optional[str] = None
+    street: Optional[str] = None
+    nearest_bus_stop: Optional[str] = None
+    latitude: Optional[str] = None
+    longitude: Optional[str] = None
+    is_primary: Optional[int] = 0
+    
+    class Config:
+        orm_mode = True
+
+
+class AuthResponseModel(BaseModel):
+    access_token: Optional[str] = None
+    user: Optional[UserModel] = None
     profile: Optional[ProfileModel] = None
     setting: Optional[SettingModel] = None
+    account: Optional[AuthAccountModel] = None
+    primary_address: Optional[AddressModel] = None
     
     class Config:
         orm_mode = True
