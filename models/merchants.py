0ea188825a05b6e3fa30c1d0f5756b42759fd65c
Jorn Kalu <kiceej@gmail.com>
2025-09-25 16:05:55 +0100

    Sept 9th 2025

--- models/merchants.py
+++ models/merchants.py
@@ -6,6 +6,7 @@ from sqlalchemy.sql.expression import and_, or_
 from sqlalchemy.sql.schema import ForeignKey
 from database.db import Base, get_laravel_datetime, get_added_laravel_datetime, compare_laravel_datetime_with_today
 from sqlalchemy.orm import relationship
+from models.merchants_users import Merchant_User
 
 
 class Merchant(Base):
